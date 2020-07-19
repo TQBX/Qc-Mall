7a8ac170b402a9ea9d7d483811c6de6acd86afa3
TQBX <hyh1332790762@163.com>
2020-07-19 22:45:30 +0800

    完成新增和修改商品的后端逻辑 采用雪花算法生成分布式id

---
+++ qc_service_goods/src/main/java/com/qingcheng/dao/CategoryBrandMapper.java
@@ -0,0 +1,10 @@
+package com.qingcheng.dao;
+
+import com.qingcheng.pojo.goods.CategoryBrand;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @author Summerday
+ */
+public interface CategoryBrandMapper extends Mapper<CategoryBrand> {
+}
